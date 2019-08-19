5daead1fdb87c73f48b37f2744cf78649cd37729
jq_lu <jq_lu@outlook.com>
2019-08-19 17:09:24 +0800

    solve #94

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [\#42 接雨水](src/stack/Sol0042.java)
 * [\#71 简化路径](src/stack/Sol0071.java)
 * [\#84 柱状图中最大的矩形](src/stack/Sol0084.java)
+* [\#94 二叉树的中序遍历](src/stack/Sol0094.java)
---
+++ src/stack/Sol0094.java
@@ -0,0 +1,51 @@
+package stack;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Sol0094 {
+
+
+   class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+
+       TreeNode(int x) {
+           val = x;
+       }
+   }
+
+    static public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> valList = new LinkedList<>();
+        if (root == null) {
+            return valList;
+        }
+        Stack<TreeNode> nodeStack = new Stack<>();
+
+        TreeNode temp = root;
+        while (temp != null) {
+            nodeStack.push(temp);
+            temp = temp.left;
+        }
+
+        while (!nodeStack.empty()) {
+            if (nodeStack.peek().right != null) {
+                temp = nodeStack.peek().right;
+                valList.add(nodeStack.pop().val);
+                while (temp != null) {
+                    nodeStack.push(temp);
+                    temp = temp.left;
+                }
+            } else {
+                valList.add(nodeStack.pop().val);
+            }
+        }
+
+        return valList;
+    }
+    public static void main(String[] args) {
+    }
+
+}
